754185c30e56a7fbd5a46f60b4e060ed1f060dbb
Chrystian Richard <crpas@discente.ifpe.edu.br>
2026-04-22 14:57:14 -0300

    first commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,88 @@
+import { Cat } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex justify-around p-7 items-center shadow-xl flex-wrap">
+        <img src="/next.svg" alt="" width={120} height={40} />
+        <div className="flex p-1 gap-7 items-center m-3">
+          <span className="cursor-pointer font-semibold">Home</span>
+          <span className="cursor-pointer font-semibold">Inicio</span>
+          <span className="cursor-pointer font-semibold">Produtos</span>
+          <span className="cursor-pointer font-semibold">Contato</span>
+        </div>
+        <div className="flex gap-6 items-center">
+          <input
+            type="text"
+            placeholder="Digite aqui"
+            className="shadow p-2 rounded-2xl outline-none"
+          ></input>
+          <span className="cursor-pointer bg-black text-white p-2 px-5 rounded-2xl hover:bg-white hover:text-black transition shadow-xl">
+            Entrar
+          </span>
+        </div>
+      </div>
+
+      <div className="flex w-screen h-150 items-center justify-center gap-20 flex-wrap">
+        <div className="m-2 flex flex-col gap-5">
+          <h1 className="text-[30px]">Olá sou chrystian estou montando</h1>
+          <p className="max-w-md text-[20px]">
+            estou altumanete cursando o curso de ads e estou tentano
+            asjdahsdhashdjhasdhjasdhhaj e nasodaosdasidiasdioaidioasdioaisd
+          </p>
+          <p className="cursor-pointer bg-black text-white p-2.5 rounded-2xl hover:bg-white hover:text-black transition shadow-xl w-fit">
+            Clique aqui!
+          </p>
+        </div>
+        <img src="/next.svg" alt="" width={420} height={120} />
+      </div>
+
+      <div className="flex flex-wrap justify-center shadow gap-8 p-10">
+        {[1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12].map((item) => (
+          <div
+            key={item}
+            className="w-[253px] h-[385px] flex flex-col justify-between p-8 rounded-2xl shadow-xl"
+          >
+            <img
+              src="/globe.svg"
+              alt="Produto"
+              className="w-full h-[120px] object-contain"
+            />
+
+            <h2 className="text-[19px] font-medium">Nome do produto</h2>
+
+            <p className="text-[17px]">R$ 19,00</p>
+
+            <button className="bg-black text-white text-center py-2 rounded-lg hover:bg-white hover:text-black border transition cursor-pointer">
+              Ver mais
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="w-screen flex justify-around items-center p-10 flex-wrap">
+        <div className="flex flex-col items-center m-2">
+          <Cat/>
+          <h1 className="text-[20px]">Escolha como pagar</h1>
+          <p className="max-w-md text-center text-[15px]">
+            Com Mercado Pago, você paga com cartão, boleto ou Pix. Você também
+            pode pagar em até 12x sem cartão com a Linha de Crédito.
+          </p>
+        </div>
+        <div className="flex flex-col items-center m-2">
+          <Cat />
+          <h1 className="text-[20px]">Frete grátis por ser sua primeira compra</h1>
+          <p className="max-w-md text-center text-[15px]">
+            Aproveite este benefício em milhões de produtos.
+          </p>
+        </div>
+        <div className="flex flex-col items-center m-2">
+          <Cat />
+          <h1 className="text-[20px]">Segurança, do início ao fim</h1>
+          <p className="max-w-md text-center text-[15px]">
+            Você não gostou do que comprou? Devolva! No Mercado Livre não há nada que você não possa fazer, porque você está sempre protegido.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,40 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div className="bg-slate-100 min-h-screen flex justify-center items-center">
+      <div className="bg-white shadow-xl w-100 h-100 rounded-sm">
+        <h1 className="text-[30px] font-semibold text-center m-6">welcome</h1>
+        <div className=" flex flex-col gap-5 m-5 mt-10">
+          <input
+            type="email"
+            placeholder="email"
+            className="w-full shadow-sm rounded-sm p-2 outline-0 focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            type="password"
+            placeholder="senha"
+            className="w-full shadow-sm rounded-sm p-2 outline-0 focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div className=" flex flex-col gap-5 m-5 mt-10 items-center">
+          <button className=" w-full bg-blue-500 text-white p-3 rounded-sm hover:bg-blue-600 transition cursor-pointer">
+            <Link 
+            href="/home">
+              Entrar 
+            </Link>
+          </button>
+          <p className="text-center text-sm mt-4">
+            cadastra-se{" "}
+            <Link
+              href="/cadastro"
+              className="text-blue-500 cursor-pointer hover:underline"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+              aqui
+            </Link>
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
