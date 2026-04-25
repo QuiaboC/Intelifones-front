f080470ea015e8cda7f554f15101040998707ef9
Chrystian Richard <crpas@discente.ifpe.edu.br>
2026-04-25 01:36:33 -0300

    ultimas alterações

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   useEffect(() => {
     axios
-      .get("https://fakestoreapi.com/products")
+      .get("colocar api")
       .then((response) => setProduto(response.data))
       .catch((error) => console.log(error));
   }, []);
@@ -21,9 +21,9 @@ export default function Home() {
   console.log(produto);
 
   const produtosFiltrados = produto.filter((item) => {
-    const matchNome = item.title.toLowerCase().includes(nome.toLowerCase());
+    const matchNome = item.nome.toLowerCase().includes(nome.toLowerCase());
 
-    const matchCategoria = category === "" || item.category === category;
+    const matchCategoria = category === "" || item.categoria.nome === category;
 
     return matchNome && matchCategoria;
   });
@@ -33,82 +33,87 @@ export default function Home() {
       <Header />
       <div className="flex w-screen h-150 items-center justify-center gap-20 flex-wrap">
         <div className="m-2 flex flex-col gap-5">
-          <h1 className="text-[30px]">Olá sou chrystian estou montando</h1>
+          <h1 className="text-[30px] font-medium">Intefones, qualidade na hora de comprar</h1>
           <p className="max-w-md text-[20px]">
-            estou altumanete cursando o curso de ads e estou tentano
-            asjdahsdhashdjhasdhjasdhhaj e nasodaosdasidiasdioaidioasdioaisd
+            A Intelifones é a melhor loja para a compra de produtos relacionados a smartphones, sempre perto da sua casa
           </p>
-          <p className="cursor-pointer bg-black text-white p-2.5 rounded-2xl hover:bg-white hover:text-black transition shadow-xl w-fit">
+          <p className="cursor-pointer bg-black text-white p-2.5 rounded-2xl hover:bg-blue-400 hover:text-white transition -xl w-fit">
             Clique aqui!
           </p>
         </div>
-        <img src="/next.svg" alt="" width={420} height={120} />
+        <img src="/vetor2.png" alt="" width={420} height={120} />
       </div>
 
-      <div className=" flex flex-row items-center gap-6">
-        <input
-          type="text"
-          value={nome}
-          onChange={(e) => {
-            setNome(e.target.value);
+      <div className="flex flex-col shadow p-5">
+        <div className=" flex flex-row items-center gap-6 px-25">
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => {
+              setNome(e.target.value);
 
-            if (e.target.value === "") {
-              setCategory("");
-            }
-          }}
-          placeholder="Nome do produto"
-          className="shadow p-2 rounded-2xl outline-none"
-        />
-
-        <div className="relative w-fit">
-          <button
-            onClick={() => setAberto(!aberto)}
+              if (e.target.value === "") {
+                setCategory("");
+              }
+            }}
+            placeholder="Nome do produto"
             className="shadow p-2 rounded-2xl outline-none"
-          >
-            Categorias
-          </button>
-          {aberto && (
-            <div className="absolute top-full left-0 mt-2 w-[220px] bg-white shadow-xl rounded-xl p-4 z-50 flex flex-col gap-3">
-              {[...new Set(produto.map((item) => item.category))].map(
-                (categoria, index) => (
-                  <span
-                    key={index}
-                    className="cursor-pointer"
-                    onClick={() => setCategory(categoria)}
-                  >
-                    {categoria}
-                  </span>
-                ),
-              )}
-            </div>
-          )}
-        </div>
-      </div>
+          />
 
-      <div className="flex flex-wrap justify-center shadow gap-8 p-10">
-        {produtosFiltrados.map((item) => (
-          <div
-            key={item.id}
-            className="w-[253px] h-[385px] flex flex-col justify-between p-8 rounded-2xl shadow-xl"
-          >
-            <img
-              src={item.image}
-              alt="Produto"
-              className="w-full h-[120px] object-contain"
-            />
-
-            <h2 className="text-[19px] font-medium truncate">{item.title}</h2>
-
-            <div className="flex flex-col gap-1">
-              <p className="text-[17px]">R$ {item.price}</p>
-              <p>Novo//semiNovo</p>
-            </div>
-
-            <button className="bg-black text-white text-center py-2 rounded-lg hover:bg-white hover:text-black border transition cursor-pointer">
-              Ver mais
+          <div className="relative w-fit">
+            <button
+              onClick={() => setAberto(!aberto)}
+              className="shadow p-2 rounded-2xl outline-none cursor-pointer"
+            >
+              Categorias
             </button>
+            {aberto && (
+              <div className="absolute top-full left-0 mt-2 w-[220px] bg-white shadow-xl rounded-xl p-4 z-50 flex flex-col gap-3">
+                {[...new Set(produto.map((item) => item.categoria.nome))].map(
+                  (categoria, index) => (
+                    <span
+                      key={index}
+                      className="cursor-pointer"
+                      onClick={() => setCategory(categoria)}
+                    >
+                      {categoria}
+                    </span>
+                  ),
+                )}
+              </div>
+            )}
           </div>
-        ))}
+        </div>
+
+        <div className="flex flex-wrap justify-center gap-8 p-10">
+          {produto.map((item) => (
+            <div
+              key={item.id}
+              className="w-[253px] h-[385px] flex flex-col justify-between p-8 rounded-2xl border"
+            >
+              <img
+                src="/logo2.png"
+                alt="Produto"
+                className="w-full h-[120px] object-contain"
+              />
+
+              <h2 className="text-[19px] font-semibold truncate">
+                {item.nome}
+              </h2>
+
+              <div className="flex flex-col gap-1">
+                <p className="text-[17px] text-blue-400">R$ {item.preco}</p>
+                <p>
+                  {item.usado ? "Usado" : "Novo"} - {item.estadoConservacao}
+                </p>
+              </div>
+
+              <button className="bg-black text-white text-center py-2 rounded-lg hover:bg-white hover:text-blue-400 border transition cursor-pointer">
+                Ver mais
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,16 @@ export default function Header() {
   return (
     <div className="flex justify-around p-7 items-center shadow-xl flex-wrap">
       <Link href="/home">
-        <img src="/next.svg" alt="" width={120} height={40} />
+        <img src="/logoNome2.png" alt="" width={160} height={40} />
       </Link>
       <div className="flex p-1 gap-7 items-center m-3">
-        <span className="cursor-pointer font-semibold">
+        <span className="cursor-pointer font-semibold hover:text-blue-400 transition text-[20px]">
           <Link href="/home">Home</Link>
         </span>
-        <span className="cursor-pointer font-semibold">Inicio</span>
-        <span className="cursor-pointer font-semibold">Produtos</span>
-        <span className="cursor-pointer font-semibold">Contato</span>
-        <span className="cursor-pointer font-semibold">
+        <span className="cursor-pointer font-semibold text-[20px] hover:text-blue-400 transition">Inicio</span>
+        <span className="cursor-pointer font-semibold text-[20px] hover:text-blue-400 transition">Produtos</span>
+        <span className="cursor-pointer font-semibold text-[20px] hover:text-blue-400 transition">Contato</span>
+        <span className="cursor-pointer font-semibold text-[20px] hover:text-blue-400 transition">
           <Link href="/Inventario">Inventario</Link>
         </span>
       </div>
@@ -22,7 +22,7 @@ export default function Header() {
           placeholder="Digite aqui"
           className="shadow p-2 rounded-2xl outline-none"
         ></input>
-        <span className="cursor-pointer bg-black text-white p-2 px-5 rounded-2xl hover:bg-white hover:text-black transition shadow-xl">
+        <span className="cursor-pointer bg-black text-white p-2 px-5 rounded-2xl hover:bg-blue-400 hover:text-white transition shadow-xl">
           Entrar
         </span>
       </div>
